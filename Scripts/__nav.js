318bd7068f61fa1d3ac438ed9373e8e568fc097a
SkopinGroup <a_skopin@icloud.com>
2023-01-18 07:32:46 +0100

    Cleaned up project, gitignore add, added images

---
+++ Scripts/__nav.js
@@ -0,0 +1,9 @@
+// let open = document.querySelector('.active');
+
+// open.addEventListener('click', () => {
+//     tl.play();
+// })
+
+// close.addEventListener('click', () => {
+//     tl.reverse();
+// })
--- Scripts/nav.js
+++
@@ -1,9 +0,0 @@
-let open = document.querySelector('.active');
-
-open.addEventListener('click', () => {
-    tl.play();
-})
-
-close.addEventListener('click', () => {
-    tl.reverse();
-})
